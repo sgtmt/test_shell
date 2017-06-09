a0eb14eba4866c620369668ef1f1cab6f50aefdc
sg_tmt <ainnuen@gmail.com>
2017-06-10 02:47:39 +0900

    Create test.sh

--- test.sh
+++ test.sh
@@ -4,12 +4,13 @@ sudo add-apt-repository ppa:ondrej/php
 sudo apt-get install python-software-properties
 sudo apt-get update
 sudo apt-get install php
-php -v 
+php -v
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+sudo chmod +x /usr/local/bin/composer
 sudo apt-get install php7.1-xml
 sudo apt-get install php7.1-mbstring
 sudo apt-get install php7.1-zip
 sudo apt-get install php7.1-mysql
 sudo apt-get install php7.1-sqlite
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-sudo chmod +x /usr/local/bin/composer
+
