a6425b2003181a893af5a9addcd1b2b8e387e40f
sg_tmt <ainnuen@gmail.com>
2017-06-10 01:43:59 +0900

    Create test.sh

--- test.sh
+++ test.sh
@@ -10,3 +10,6 @@ sudo apt-get install php7.1-mbstring
 sudo apt-get install php7.1-zip
 sudo apt-get install php7.1-mysql
 sudo apt-get install php7.1-sqlite
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+sudo chmod +x /usr/local/bin/composer
