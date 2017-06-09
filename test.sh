3929530a78882a3dfe40da07d612052f70d8731b
sg_tmt <ainnuen@gmail.com>
2017-06-10 01:32:48 +0900

    Create test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,12 @@
-
+#!/bin/sh
+sudo apt-get update
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get install python-software-properties
+sudo apt-get update
+sudo apt-get install php
+php -v 
+sudo apt-get install php7.1-xml
+sudo apt-get install php7.1-mbstring
+sudo apt-get install php7.1-zip
+sudo apt-get install php7.1-mysql
+sudo apt-get install php7.1-sqlite
